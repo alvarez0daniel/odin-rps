e9c7cc3385c4a2292c0143effacef8d399b63ca9
Daniel Alvarez <alvarez0daniel@gmail.com>
2022-02-22 10:49:29 -0800

    added function that returns a random element from an array that contains ['ROCK', 'PAPER', 'SCISSORS']

--- rps.js
+++ rps.js
@@ -1,5 +1,8 @@
 // display a welcome message to the user
 const welcomeMessage = () => console.log('Welcome to my Rock, Paper, Scissors game! You will enter your selection into the prompt and then the computer will choose. May the best man or machine win!');
 
-
+// take in array of options and computer chooses rock, paper, or scissors randomly; returns selection
+const computerPlay = ( optionsArr ) => {
+  return optionsArr[ Math.floor( Math.random() * 3 ) ];
+};
 
