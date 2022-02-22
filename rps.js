ac88c1532504c7146fdfcefab060f06624a903d6
Daniel Alvarez <alvarez0daniel@gmail.com>
2022-02-22 10:47:19 -0800

    added welcome message that logs into console

---
+++ rps.js
@@ -0,0 +1,5 @@
+// display a welcome message to the user
+const welcomeMessage = () => console.log('Welcome to my Rock, Paper, Scissors game! You will enter your selection into the prompt and then the computer will choose. May the best man or machine win!');
+
+
+
